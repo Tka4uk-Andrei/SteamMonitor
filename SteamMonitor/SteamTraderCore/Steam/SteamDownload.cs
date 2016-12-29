fac538faed5202c3318a0ba295968a986d61b85a
Andrey Tkachuk <tka4uk.andrei@yandex.ru>
2016-12-29 21:11:09 +0200

    IDownload removed

--- SteamMonitor/SteamTraderCore/Steam/SteamDownload.cs
+++ SteamMonitor/SteamTraderCore/Steam/SteamDownload.cs
@@ -5,7 +5,7 @@ using Steam_monitor;
 
 namespace SteamMonitor.SteamTraderCore.Steam
 {
-    public class SteamDownload : IDownload
+    public class SteamDownload
     {
         private const string STEAM_MARKET_ITEM =
             "http://steamcommunity.com/market/search/render/?query={0}&start={1}&count=100&curency=5&appid=440&sort_column=name&sort_dir=asc";
