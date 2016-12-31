d2431a8cd126b8c7311ad9ce12763bf256e5909b
Andrey Tkachuk <tka4uk.andrei@yandex.ru>
2016-12-31 19:26:38 +0200

    add claces for work

---
+++ SteamMonitor/SteamAdditionalInfo/Additionalnfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Packaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SteamMonitor.SteamAdditionalInfo
+{
+    class Additionalnfo
+    {
+        public string Name { get; set; }
+        public int SoldCountPerDay { get; set; }
+    }
+}
---
+++ SteamMonitor/SteamAdditionalInfo/SteamParcer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SteamMonitor.SteamAdditionalInfo
+{
+    static class SteamParcer
+    {
+        public static Additionalnfo Parce(StreamReader response)
+        {
+            var item = new Additionalnfo();
+
+
+
+            return item;
+        }
+
+    }
+}
