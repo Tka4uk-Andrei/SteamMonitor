1fb13fe089ffea7a4a39dcc1808044dc7b11fdb1
Andrey Tkachuk <tka4uk.andrei@yandex.ru>
2017-06-23 19:41:19 +0200

    refactored KeyPriceData.cs

--- SteamMonitor/Data/KeyPriceData.cs
+++ SteamMonitor/Data/KeyPriceData.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SteamMonitor.Data
+﻿namespace SteamMonitor.Data
 {
-    class KeyPriceData
+    public class KeyPriceData
     {
         public double BuyPrice { get; set; }
         public double SellPrice { get; set; }
